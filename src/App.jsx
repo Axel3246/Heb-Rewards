4877eb8f3bc7f9e70e4cc1d4ed1312e7b05d2616
Naexwk <gomezdeharomarcel11@gmail.com>
2023-04-26 17:55:30 -0600

    Cambios minimos del mapa

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,11 @@ import "./styles.css";
 export default function App() {
   const [count, setCount] = useState(0)
 
-//  const { isLoaded } = useLoadScript({
-//    googleMapsApiKey: "AIzaSyCVLU4FFbvQ8g88L619Kj6nQ4YF0Bexrwg" // Add your API key
-//  });
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyCVLU4FFbvQ8g88L619Kj6nQ4YF0Bexrwg" // Add your API key
+  });
   
-//  return isLoaded ? <Maps/> : null;
-//      {/* <Home/> */}
-//      {/* <Login/> */}
-  return (
-    <Map/>
-
-  )  
+  return isLoaded ? <Maps/> : null;
+      {/* <Home/> */}
+      {/* <Login/> */}
 }
