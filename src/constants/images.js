768c073c96609079489c0ca0292d653219f7a580
Axel HC <hplaxel@gmail.com>
2023-04-28 18:05:03 -0600

    Update images.js

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,7 @@
 import koop from '../assets/img/repo/logokoop2.png'
 import heb from '../assets/img/landing/heb.png'
 import heb_svg from '../assets/H-E-B_logo.svg'
-import holder_koopas from '../assets/img/landing/holder_koopas.jpg'
+import holder_koopas from '../assets/img/landing/Holder_Koopas.jpg'
 import wave from '../assets/img/landing/wave.png'
 
 export default {
