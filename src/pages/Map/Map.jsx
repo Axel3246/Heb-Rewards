57a2b7f69d3dc5a31062f5fdde853f8fc79811cb
ricardordz13 <ricardo.rdz.trevino@gmail.com>
2023-05-03 16:54:28 -0500

    Update Map.jsx

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -25,21 +25,25 @@ console.log(latitude + " " + longitude);
 
 // función principal
 function Map() {
-  var options = {
+
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 8,
     center: {lat: latitude, lng: longitude},
-    zoom: 8
-  }
+  });
+
+  const marker = new google.maps.Marker({
+    position: {lat: latitude, lng: longitude},
+    map: map,
+  });
     
   map = new google.maps.Map(document.getElementById("map"),options)
 
   return (
     <>
       <h1>Mapa</h1>
-
       <div id="map"></div>
-
-      <script defer
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCVLU4FFbvQ8g88L619Kj6nQ4YF0Bexrwg&callback=initMap">
+      <script async
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCVLU4FFbvQ8g88L619Kj6nQ4YF0Bexrwg&callback=Map">
       </script>
     </>
   );
