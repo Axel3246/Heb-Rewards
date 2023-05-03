e6e24b14d1db0b61af8848786fbe299e7ebe5f28
ricardordz13 <ricardo.rdz.trevino@gmail.com>
2023-05-03 13:34:53 -0500

    Update Map.jsx

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -1,17 +1,16 @@
 import React, { useState } from "react";
 import { GoogleMap, InfoWindow, Marker } from "@react-google-maps/api";
 
-let latitude;
-let longitude;
-
 const status = document.querySelector('.status');
 
 const success = (position) => {
+  let latitude;
   console.log(position);
   return latitude = position.coords.latitude;
 }
 
 const successL = (position) => {
+  let longitude;
   console.log(position);
   return longitude = position.coords.longitude;
 }
