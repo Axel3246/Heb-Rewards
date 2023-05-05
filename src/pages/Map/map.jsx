3350b72902b71cb915811bcf09cc8548a2920c8a
ricardordz13 <ricardo.rdz.trevino@gmail.com>
2023-05-05 09:28:21 -0500

    Update map.jsx

--- src/pages/Map/map.jsx
+++ src/pages/Map/map.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from "react";
+import { GoogleMap, InfoWindow, Marker, useLoadScript } from "@react-google-maps/api";
+
 import './map.css'
 import { images } from '../../constants'
 import AppBarHome from '../Home/components/AppBarHome'
@@ -14,7 +16,94 @@ const mapTheme = createTheme({
     }
 })
 
+// se inicializan las variables para la geolocalización
+let latitude;
+let longitude;
+
+// geolocalización
+const status = document.querySelector('.status');
+
+const success = (position) => {
+  console.log(position);
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude;
+}
+
+const error = () => {
+  status.textContent = 'Échale ganas'
+}
+
+// obtener la posición actual
+navigator.geolocation.getCurrentPosition(success, error);
+
 const map = () => {
+    const [activeMarker, setActiveMarker] = useState(null);
+
+    // API Key de Google
+    const { isLoaded } = useLoadScript ({
+      googleMapsApiKey: "AIzaSyCVLU4FFbvQ8g88L619Kj6nQ4YF0Bexrwg"
+    });
+    
+    if (!isLoaded) {
+      return "Hay un error"
+    }
+    
+    // ubicación de cada HEB
+    const markers = [
+      {
+        id: 1,
+        name: "HEB Contry",
+        position: { lat: 25.6271687, lng: -100.2750425 }
+      },
+      {
+        id: 2,
+        name: "HEB Cumbres",
+        position: { lat: 25.73329797, lng: -100.3978786 }
+      },
+      {
+        id: 3,
+        name: "HEB Del Valle",
+        position: { lat: 25.6505631, lng: -100.3608711 }
+      },
+      {
+        id: 4,
+        name: "HEB Estanzuela",
+        position: { lat: 25.58734297, lng: -100.258748 }
+      },
+      {
+        id: 5,
+        name: "Yo",
+        position: { lat: latitude, lng: longitude }
+      }
+    ];
+  
+    // Posición actual
+    console.log(latitude + " " + longitude);
+    const center = { lat: latitude, lng: longitude }
+  
+    const handleActiveMarker = (marker) => {
+      if (marker === activeMarker) {
+        return;
+      }
+      setActiveMarker(marker);
+    };
+  
+    function resolveAfter2Seconds(x) {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve(x);
+        }, 2000);
+      });
+    }
+  
+    // poner marcadores
+    const handleOnLoad = async (map) => {
+      const bounds = new google.maps.LatLngBounds();
+      markers.forEach(({ position }) => bounds.extend(position));
+      map.fitBounds(bounds);
+      var x = await resolveAfter2Seconds(10);
+    };
+
     return (
         <>
             <ThemeProvider theme={mapTheme} >
@@ -35,7 +124,28 @@ const map = () => {
                             </Typography>
                         </Box>
                         <Box>
-                          <iframe width="800px" height="400px" src="https://clausa.app.carto.com/map/cf8f3d90-1991-4dea-8304-632ac360c9b7"></iframe>
+                            <GoogleMap
+                                onLoad={ handleOnLoad }
+                                onClick={() => setActiveMarker(null)}
+                                center={ center }
+                                zoom={ 15 }
+                                mapContainerStyle={{ width: "100vw", height: "100vh" }}
+
+                                >
+                                {markers.map(({ id, name, position }) => (
+                                <Marker
+                                    key={ id }
+                                    position={ position }
+                                    onClick={() => handleActiveMarker(id)}
+                                >
+                                    {activeMarker === id ? (
+                                    <InfoWindow onCloseClick={() => setActiveMarker(null)}>
+                                        <div>{name}</div>
+                                    </InfoWindow>
+                                    ) : null}
+                                </Marker>
+                                ))}
+                            </GoogleMap>
                         </Box>
 
                     </Box>
