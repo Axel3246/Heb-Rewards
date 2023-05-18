5a2cd7dcc43b8c6b3e6534068248d0d57033ba5c
Axel HC <hplaxel@gmail.com>
2023-05-17 19:21:09 -0600

    Some Additions to Formatting

--- src/pages/Juego_Recetas/JuegoRecetas.jsx
+++ src/pages/Juego_Recetas/JuegoRecetas.jsx
@@ -3,13 +3,10 @@ import { images } from '../../constants'
 import '../Home/Home.css'
 import AppBarHome from '../Home/components/AppBarHome.jsx'
 import { database, auth, signInWithGoogle } from '../../FirebaseConfig'
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { Box } from '@mui/material'
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
 import { useEffect, useState } from 'react';
@@ -31,45 +28,44 @@ const Item = styled(Paper)(({ theme }) => ({
 
 function JuegoRecetas() {
     const [rows, setRows] = useState([])
-    
+
     useEffect(() => {
         const coll = collection(database, 'Recetas');
         const unsuscribe = onSnapshot(coll, querySnapshot => {
             setRows(
-              querySnapshot.docs.map(doc => ({
-                id: doc.id,
-                url_imagen: doc.data().imagen, 
-                nombre: doc.data().nombre, 
-                productos: doc.data().productos
-              })
-            )
-      
-          )})
-            return unsuscribe;
-    },[])
+                querySnapshot.docs.map(doc => ({
+                    id: doc.id,
+                    url_imagen: doc.data().imagen,
+                    nombre: doc.data().nombre,
+                    productos: doc.data().productos
+                })
+                )
 
+            )
+        })
+        return unsuscribe;
+    }, [])
     console.log(rows)
     //rows.forEach(element => console.log(element));
-    
-    return (
-        
-        <>
 
-            <AppBarHome />
-            <Container>
-                
-                <Grid
-                    sx={{ mt: 18 }}
-                    container
-                    justifyContent="space-evenly"
-                    spacing={4}
-                >
-                    <TarjetaReceta details={rows} />
-                </Grid>
+    return (
+        <>
+            <Container sx={{ alignItems: 'center' }}>
+                <AppBarHome />
+                <Box maxWidth='sm' sx={{ mt: 15 }}>
+                <Typography variant='h4' sx={{ fontWeight: 'bold' }}><span>Dine</span> n' Dash!</Typography>
+                <Typography variant='h4' sx={{ fontWeight: '1', fontSize: 16, mb: 4 }}>Elige, escanea y <span>¡gana!</span></Typography>
+                    <Grid
+                        container
+                        justifyContent="space-evenly"
+                        spacing={4}
+                        alignItems='center'
+                    >
+                        <TarjetaReceta details={rows} />
+                    </Grid>
+                </Box>
             </Container >
         </>
-            
-
     )
 }
 
--- src/pages/Juego_Recetas/tarjetaReceta.jsx
+++ src/pages/Juego_Recetas/tarjetaReceta.jsx
@@ -5,6 +5,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import Divider from '@mui/material/Divider';
 
 import Grid from '@mui/material/Grid';
 
@@ -14,16 +15,17 @@ const TarjetaReceta = (props) => {
         <>
             {props.details.map((value, index) => (
               <Grid item sx={{ textAlign: 'center', }}>
-              <Card sx={{ maxWidth: 345 }} key={index}>
+              <Card sx={{ width: 270 }} key={index}>
                   <CardActionArea href='/receta'>
                       <CardMedia
                           component="img"
-                          sx={{ height: 240 }}
+                          sx={{ height: 210 }}
                           image={value.url_imagen}
-                          object-fit="contain"
+                          object-fit="fill"
                       />
                       <CardContent>
-                          <Typography gutterBottom variant="h5" component="div">
+                      <Divider sx={{width:250, mb:2}}></Divider>
+                          <Typography gutterBottom variant="h5" component="div" sx={{fontWeight: 'bold'}}>
                               {value.nombre}
                           </Typography>
                       </CardContent>
