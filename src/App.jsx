646c1b2bd705acbab204e63422f21d198d13f14a
Axel HC <hplaxel@gmail.com>
2023-05-25 08:07:39 -0600

    Para Ekipo

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Map from './pages/Map/map'
 import Scan from './pages/Scan/scan'
 import Productos from './pages/Productos/products'
 import Recetas from './pages/Juego_Recetas/Recetas'
-
+import WhatsTP from './pages/WhatsTP/WhatsTP'
 
 import Receta from './pages/Receta/receta'
 
@@ -36,6 +36,7 @@ export default function App() {
           <Route path="/recetas" element={<Recetas/>}></Route>
 
           <Route path="/Receta" element={<Receta/>}></Route>
+          <Route path="/WhatsTP" element={<WhatsTP/>}></Route>
 
         </Routes>
       </Router> 
---
+++ src/pages/WhatsTP/WhatsTP.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import WTPAppBar from '../WhatsTP/components/WTPAppBar'
+import { Container } from '@mui/material';
+
+const WhatsTP = () => {
+  return (
+    <Container>
+        <WTPAppBar/>
+        <h1>Hello</h1>
+    </Container>
+  )
+}
+
+export default WhatsTP
